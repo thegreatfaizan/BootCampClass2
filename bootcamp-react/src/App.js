9770ca2f6242d8c6aadf1581a241b22266f66aaf
thegreatfaizan <faizanwaseem476@gmail.com>
2020-06-09 14:08:28 +0500

    Created a new repository named "BootCampClass2"

---
+++ bootcamp-react/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This my first React App
+          <br />Stay Tuned!
+        </p>
+      </header>
+      <footer>
+        <p>
+          Created by <a href="https://www.instagram.com/tgf476">The Great Faizan</a>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
